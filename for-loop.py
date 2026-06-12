852d726e5515347467470adab0f0412674ec73aa
Shahriar Alif <fahim.shahriar.alif@gmail.com>
2026-06-12 13:46:14 +0600

    For loop

---
+++ for-loop.py
@@ -0,0 +1,8 @@
+x = int(input("Enter upper limit : "))
+y = int(input("Enter lower limit : "))
+
+for i in range(x,y):
+    print(i , end = '  ')
+#This is a for loop for print until the range the range is given . Here x is the upper limit and y is the lower limit
+#The code will take x as starting period and y as ending like it will print until the y like if y =6 , it will print till 5
+

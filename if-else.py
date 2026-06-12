3a317b0ba4e8de64f8e82d9a03008ae8eafef5ce
Shahriar Alif <fahim.shahriar.alif@gmail.com>
2026-06-12 11:19:30 +0600

    If else Statement

--- if-else.py
+++ if-else.py
@@ -7,3 +7,11 @@ else:
     x = 0
 
 print(x)
+
+x = int (input("Enter a number: "))
+if x > 0 :
+    print(x ,"is the positive number")
+elif x < 0 :
+    print(x ,"is the negative number")
+else:
+    print(x ,"is the whole number")
